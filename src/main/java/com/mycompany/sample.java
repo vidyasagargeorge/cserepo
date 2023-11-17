955970fbac02faa0936bc11e66e2ceaeef61de49
vidyasagargeorge <georgegeos143@gmail.com>
2023-11-17 10:58:55 +0530

    Second commit

--- src/main/java/com/mycompany/sample.java
+++ src/main/java/com/mycompany/sample.java
@@ -1,7 +1,7 @@
 package com.mycompany;
 
-public  class Hellpoworld {
+public  class Helloworld {
     public static void main(string[] args){
-        system.out.println("This is my first Hello program");
+        system.out.println("Good morining guys");
     }
 }
